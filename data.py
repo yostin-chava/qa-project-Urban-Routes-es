58efca78ad5ec5049a8434f3d8899dc6ef4d3133
Yostin Chavarria <yostin.repa@gmail.com>
2025-06-13 08:06:32 -0600

    Correcion proyecto, se agrego el archivo py helpers donde contiene la funcion para interceptar el codigo y se agrego el archivo pages que contiene los localizadores y las interacciones

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-cc8ea5fa-5253-47ac-989f-f58bf66e6c56.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-6749fa6c-6b78-469d-a878-efedf1a26af2.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
